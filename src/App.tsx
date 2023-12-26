c191191dfb9b311ef1d49d0c3ec19015524d9e4b
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2023-12-26 08:39:41 +0600

    folder, header footer, and banner done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Home from "./pages/open/Home";
+
+function App() {
+  return (
+    <main className="w-full">
+      <Home />
+    </main>
+  );
+}
+
+export default App;
